7a69d942579f526627800c53de213a54b2fa77eb
Thiao Passadore <TP351317@fi365.ort.edu.uy>
2026-05-07 21:45:05 -0300

    agrego consola y mvc

---
+++ WebApplicationObligatorio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationObligatorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
