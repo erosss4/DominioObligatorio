1428a11c691218a11fe2cbbd973b3f62de5441d6
Rodrigo Sangorrin <rodrigo44441@gmail.com>
2026-05-10 01:13:40 -0300

    Se agregan metodos de alta en Sistema.cs

--- DominioObligatorio/Sistema.cs
+++ DominioObligatorio/Sistema.cs
@@ -6,8 +6,69 @@ namespace DominioObligatorio
 {
     public class Sistema
     {
-        private List<Persona> personas = new List<Persona>();
-        private List<Activo> activos = new List<Activo>();
-        private List<Incidente> incidentes = new List<Incidente>();
+        private List<Persona> _personas = new List<Persona>();
+        private List<Activo> _activos = new List<Activo>();
+        private List<Incidente> _incidentes = new List<Incidente>();
+
+
+        // ### Falta en el Astah ### -- CrearActivo
+        public void CrearActivo(Activo a)
+        {
+            if (a == null) throw new Exception("El activo no puede ser nulo");
+            _activos.Add(a);
+        }
+
+        // ### Falta en el Astah ### -- CrearIncidente
+        public void CrearIncidente(Incidente i)
+        {
+            if (i == null) throw new Exception("El incidente no puede ser nulo");
+            _incidentes.Add(i);
+        }
+        
+        public void CrearPersona(Persona p)
+        {
+            if (p == null) throw new Exception("La persona no puede ser nula");
+            p.Validar();
+            if (_personas.Contains(p)) throw new Exception("La cedula ya existe");
+            _personas.Add(p);
+        }
+        
+        public List<Persona> ObtenerPersonasConActivos()
+        {
+            return _personas;
+        }
+        
+        public List<Activo> ObtenerActivosDePersona(Persona p)
+        {
+            if (p == null) throw new Exception("La persona no puede ser nula");
+
+            List<Activo> resultado = new List<Activo>();
+            foreach (Activo a in _activos)
+            {
+                foreach (Cuenta c in p.Cuentas)
+                {
+                    if (c.Activos.Contains(a))
+                    {
+                        resultado.Add(a);
+                        break;
+                    }
+                }
+            }
+            return resultado;
+        }
+
+        public List<Activo> ActivosSinBackup()
+        {
+            List<Activo> resultado = new List<Activo>();
+            foreach (Activo a in _activos)
+            {
+                if (!a.TieneBackup)
+                    resultado.Add(a);
+            }
+            return resultado;
+        }
+        
+       
     }
+
 }
